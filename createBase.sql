000d12b8abec96acd66019d1a824cfe885ecb233
Zelfrost <deconinck.damien@gmail.com>
2014-01-12 19:39:16 +0100

    Petit correctif sur le menu et le create_sql

--- createBase.sql
+++ createBase.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS    transactions;
 
 
 CREATE TABLE  users (
-    idUser                      INT AUTO_INCREMENT,
+    idUser                      INT,
     nom                         VARCHAR(30),
     prenom                      VARCHAR(30),
     login                       VARCHAR(20),
@@ -15,19 +15,19 @@ CREATE TABLE  users (
     CONSTRAINT pk_user          PRIMARY KEY (idUser)
 );
 
-INSERT INTO users(nom, prenom, login, pass, argent, marketMaker) 
-    VALUES('Deconinck', 'Damien', 'deconind', 'moi', '10000', '1');
-INSERT INTO users(nom, prenom, login, pass, argent, marketMaker) 
-    VALUES('Godart', 'Christophe', 'godartc', 'moi', '10000', '1');
-INSERT INTO users(nom, prenom, login, pass, argent, marketMaker) 
-    VALUES('Vanhoutte', 'Mickael', 'vanhoutm', 'moi', '10000', '0');
-INSERT INTO users(nom, prenom, login, pass, argent, marketMaker) 
-    VALUES('Weng', 'Tom', 'wengh', 'moi', '10000', '0');
+INSERT INTO users(idUser, nom, prenom, login, pass, argent, marketMaker) 
+    VALUES(0, 'Deconinck', 'Damien', 'deconind', 'moi', '10000', 1);
+INSERT INTO users(idUser, nom, prenom, login, pass, argent, marketMaker) 
+    VALUES(1, 'Godart', 'Christophe', 'godartc', 'moi', '10000', 1);
+INSERT INTO users(idUser, nom, prenom, login, pass, argent, marketMaker) 
+    VALUES(2, 'Vanhoutte', 'Mickael', 'vanhoutm', 'moi', 10000, 0);
+INSERT INTO users(idUser, nom, prenom, login, pass, argent, marketMaker) 
+    VALUES(3, 'Weng', 'Tom', 'wengh', 'moi', 10000, 0);
 
 
 CREATE TABLE markets
 (
-    idMarket                    INT AUTO_INCREMENT,
+    idMarket                    INT,
     libelle                     VARCHAR(300),
     libelleInverse              VARCHAR(300),
     dateFin                     DATE,
@@ -36,15 +36,15 @@ CREATE TABLE markets
     CONSTRAINT pk_market        PRIMARY KEY (idMarket)
 );
 
-INSERT INTO markets(libelle, libelleInverse, dateFin, publication) 
-    VALUES('Monsieur Beaufils mettra un 20 en projet', 'Monsieur Beaufils ne mettra aucun 20 en projet', '2014-04-01',  CURRENT_TIMESTAMP);
-INSERT INTO markets(libelle, libelleInverse, dateFin, publication) 
-    VALUES('Demode arrivera en retard a tous les cours cette annee', 'Demode arrivera a temps a au moins un cours cette annee', '2014-07-01',  CURRENT_TIMESTAMP);
+INSERT INTO markets(idMarket, libelle, libelleInverse, dateFin, publication) 
+    VALUES(0, 'Monsieur Beaufils mettra un 20 en projet', 'Monsieur Beaufils ne mettra aucun 20 en projet', '2014-04-01',  CURRENT_TIMESTAMP);
+INSERT INTO markets(idMarket, libelle, libelleInverse, dateFin, publication) 
+    VALUES(1, 'Demode arrivera en retard a tous les cours cette annee', 'Demode arrivera a temps a au moins un cours cette annee', '2014-07-01',  CURRENT_TIMESTAMP);
 
 
 CREATE TABLE transactions
 (
-    idTrans                     INT AUTO_INCREMENT,
+    idTrans                     INT,
     marketID                    INT,
     userID                      INT,
     nombre                      NUMERIC(10),
@@ -62,15 +62,15 @@ CREATE TABLE transactions
                                 ON DELETE SET NULL
 );
 
-INSERT INTO transactions(userID, marketID, nombre, prix, choix, dateTrans) 
-    VALUES(0, 0, 10, 50, 0, CURRENT_TIMESTAMP);
-INSERT INTO transactions(userID, marketID, nombre, prix, choix, dateTrans) 
-    VALUES(0, 1, 60, 60, 1, CURRENT_TIMESTAMP);
-INSERT INTO transactions(userID, marketID, nombre, prix, choix, dateTrans) 
-    VALUES(0, 2, 40, 80, 1, CURRENT_TIMESTAMP);
-INSERT INTO transactions(userID, marketID, nombre, prix, choix, dateTrans) 
-    VALUES(0, 3, 20, 80, 1, CURRENT_TIMESTAMP);
-INSERT INTO transactions(userID, marketID, nombre, prix, choix, dateTrans) 
-    VALUES(0, 0, 70, 90, 1, CURRENT_TIMESTAMP);
-INSERT INTO transactions(userID, marketID, nombre, prix, choix, dateTrans) 
-    VALUES(0, 1, 20, 30, 1, CURRENT_TIMESTAMP);
+INSERT INTO transactions(idTrans, userID, marketID, nombre, prix, choix, dateTrans) 
+    VALUES(0, 0, 0, 10, 50, 0, CURRENT_TIMESTAMP);
+INSERT INTO transactions(idTrans, userID, marketID, nombre, prix, choix, dateTrans) 
+    VALUES(1, 0, 1, 60, 60, 1, CURRENT_TIMESTAMP);
+INSERT INTO transactions(idTrans, userID, marketID, nombre, prix, choix, dateTrans) 
+    VALUES(2, 0, 2, 40, 80, 1, CURRENT_TIMESTAMP);
+INSERT INTO transactions(idTrans, userID, marketID, nombre, prix, choix, dateTrans) 
+    VALUES(3, 0, 3, 20, 80, 1, CURRENT_TIMESTAMP);
+INSERT INTO transactions(idTrans, userID, marketID, nombre, prix, choix, dateTrans) 
+    VALUES(4, 0, 0, 70, 90, 1, CURRENT_TIMESTAMP);
+INSERT INTO transactions(idTrans, userID, marketID, nombre, prix, choix, dateTrans) 
+    VALUES(5, 0, 1, 20, 30, 1, CURRENT_TIMESTAMP);
