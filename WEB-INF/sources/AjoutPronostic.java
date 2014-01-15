0e5c928e39b846905eefe5aca1b6dcc3a84610c1
Zelfrost <deconinck.damien@gmail.com>
2014-01-15 10:24:26 +0100

    Ajout creation prono

---
+++ WEB-INF/sources/AjoutPronostic.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.sql.*;
+import javax.sql.*;
+import javax.naming.*;
+
+@WebServlet("/AjoutPronostic")
+public class AjoutPronostic extends HttpServlet
+{
+	public void service( HttpServletRequest req, HttpServletResponse res )
+		throws ServletException, IOException
+	{
+		if (req.getParameter("libelle")!=null
+			&& req.getParameter("libelleInverse")!=null
+			&& req.getParameter("dateFin")!=null ) {
+
+
+			PreparedStatement pst = null;
+
+		    String insert = "INSERT INTO markets SELECT MAX(idMarket)+1, ?, ?, ?, date('now') FROM markets;";
+
+		    try {
+				Context initCtx = 	new InitialContext();
+	            Context envCtx 	= 	(Context) initCtx.lookup("java:comp/env");
+	            DataSource ds 	= 	(DataSource) envCtx.lookup("base");
+	            Connection con 	= 	ds.getConnection();
+
+		        pst = con.prepareStatement(insert);
+
+		        pst.setString(1, req.getParameter("libelle"));
+		        pst.setString(2, req.getParameter("libelleInverse"));
+		        pst.setString(3, req.getParameter("dateFin"));
+
+      			pst.executeUpdate();
+
+      			con.close();
+		    } catch (Exception e ) {}
+		}
+	}
+}
