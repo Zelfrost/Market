35704e9a0806037b5db6eb6200cdbef2a5a2ab2c
Christophe-Godart <ch.godart59930@gmail.com>
2014-01-22 10:19:54 +0100

    correctif de sécurité

--- WEB-INF/sources/AcheterBons.java
+++ WEB-INF/sources/AcheterBons.java
@@ -9,58 +9,73 @@ import javax.naming.*;
 @WebServlet("/AcheterBons")
 public class AcheterBons extends HttpServlet
 {
-	public void service( HttpServletRequest req, HttpServletResponse res )
-		throws ServletException, IOException
-	{
+    public void service( HttpServletRequest req, HttpServletResponse res )
+	throws ServletException, IOException
+    {
+	
+	try{
+	    int prix = (int) Integer.parseInt(req.getParameter("prixBons"));
+	    if (prix <= 0 || prix >= 100)
+		res.sendRedirect("information?id=" + req.getParameter("id") + "&choix=" + req.getParameter("choix") + "&error=3");
+	    //prix <0 ou >100
 
-		if (req.getParameter("nbBons") != null && req.getParameter("prixBons") != null)
-		{
-			String login 			= req.getUserPrincipal().getName();
-
-			try {
-					Context initCtx = new InitialContext();
-		          	Context envCtx 	= (Context) initCtx.lookup("java:comp/env");
-	            	DataSource ds 	= (DataSource) envCtx.lookup("base");
-	            	Connection con 	= ds.getConnection();
-
-	            	Statement st 	= con.createStatement();
-	            	ResultSet rs 	= st.executeQuery("SELECT idUser, argent FROM users WHERE login='" + req.getUserPrincipal().getName() + "'");
-	            	rs.next();
-
-	            	String idUser	= rs.getString("idUser");
-	            	int somme		= Integer.parseInt(req.getParameter("nbBons")) * Integer.parseInt(req.getParameter("prixBons"));
-	            	int nbBons		= Integer.parseInt(req.getParameter("nbBons"));
-
-	            	if(rs.getInt("argent") >= somme) {
-	            		rs 			= st.executeQuery("SELECT idTrans, userID, 100-prix AS prix ,nombreRestant FROM transactions WHERE 100-prix <= " + req.getParameter("prixBons") + " AND choix = " + ((Integer.parseInt(req.getParameter("choix"))==0)?1:0) + " ORDER BY prix DESC;");
-	            		if(rs.next()) {
-	            			Statement upST	= con.createStatement();
-	            			int retraitBons;
-	            			do {
-	            				retraitBons	= (nbBons > rs.getInt("nombreRestant")?rs.getInt("nombreRestant"):nbBons);
-	            				upST		= con.createStatement();
-	            				upST.executeUpdate("INSERT INTO transactions SELECT MAX(idtrans)+1, " + req.getParameter("id") + ", " + idUser + ", " + retraitBons + ", 0, " + rs.getInt("prix") + ", " + req.getParameter("choix") + ", CURRENT_TIMESTAMP FROM transactions;");
-	            				upST.executeUpdate("UPDATE transactions SET nombreRestant = nombreRestant - " + retraitBons + " WHERE idTrans = " + rs.getString("idTrans"));
-	            				upST.executeUpdate("UPDATE users SET argent = argent - " + ((100-rs.getInt("prix")) * retraitBons) + " WHERE idUser = " + rs.getString("userID"));
-	            				upST.executeUpdate("UPDATE users SET argent = argent - " + (rs.getInt("prix") * retraitBons) + " WHERE idUser = " + idUser);
-	            				nbBons		-= retraitBons;
-	            				if(nbBons == 0)
-	            					break;
-	            			} while(rs.next());
-	            			if(nbBons != 0)
-	            				st.executeUpdate("INSERT INTO transactions SELECT MAX(idtrans)+1, " + req.getParameter("id") + ", " + idUser + ", " + nbBons + ", " + nbBons + ", " + req.getParameter("prixBons") + ", " + req.getParameter("choix") + ", CURRENT_TIMESTAMP FROM transactions;");
-	            		} else {
-			            	st.executeUpdate("INSERT INTO transactions SELECT MAX(idtrans)+1, " + req.getParameter("id") + ", " + idUser + ", " + req.getParameter("nbBons") + ", " + req.getParameter("nbBons") + ", " + req.getParameter("prixBons") + ", " + req.getParameter("choix") + ", CURRENT_TIMESTAMP FROM transactions;");
-			            }
-	      				con.close();
-	      				res.sendRedirect("information?id=" + req.getParameter("id") + "&choix=" + req.getParameter("choix") + "&success=1");
-	      			} else {
-      					con.close();
-	      				res.sendRedirect("information?id=" + req.getParameter("id") + "&choix=" + req.getParameter("choix") + "&error=1");
-	      			}
-		    	} catch (Exception e ) {
-		    		e.printStackTrace(res.getWriter());
-		    	}			
-		}
+	} catch (Exception e){
+	    res.sendRedirect("information?id=" + req.getParameter("id") + "&choix=" + req.getParameter("choix") + "&error=2"); 
+	    //format prix invalide
 	}
+
+
+	if (req.getParameter("nbBons") != null && req.getParameter("prixBons") != null)
+	    {
+		String login 			= req.getUserPrincipal().getName();
+
+		try {
+		    
+		    Context initCtx = new InitialContext();
+		    Context envCtx 	= (Context) initCtx.lookup("java:comp/env");
+		    DataSource ds 	= (DataSource) envCtx.lookup("base");
+		    Connection con 	= ds.getConnection();
+
+		    Statement st 	= con.createStatement();
+		    ResultSet rs 	= st.executeQuery("SELECT idUser, argent FROM users WHERE login='" + req.getUserPrincipal().getName() + "'");
+		    rs.next();
+
+		    String idUser	= rs.getString("idUser");
+		    
+		    int somme		= Integer.parseInt(req.getParameter("nbBons")) * Integer.parseInt(req.getParameter("prixBons"));
+		    int nbBons		= Integer.parseInt(req.getParameter("nbBons"));
+		    
+		   
+		    if(rs.getInt("argent") >= somme) {
+			rs 			= st.executeQuery("SELECT idTrans, userID, 100-prix AS prix ,nombreRestant FROM transactions WHERE 100-prix <= " + req.getParameter("prixBons") + " AND choix = " + ((Integer.parseInt(req.getParameter("choix"))==0)?1:0) + " AND marketID=" + req.getParameter("id") + " ORDER BY prix DESC;");
+			if(rs.next()) {
+			    Statement upST	= con.createStatement();
+			    int retraitBons;
+			    do {
+				retraitBons	= (nbBons > rs.getInt("nombreRestant")?rs.getInt("nombreRestant"):nbBons);
+				upST		= con.createStatement();
+				upST.executeUpdate("INSERT INTO transactions SELECT MAX(idtrans)+1, " + req.getParameter("id") + ", " + idUser + ", " + retraitBons + ", 0, " + rs.getInt("prix") + ", " + req.getParameter("choix") + ", CURRENT_TIMESTAMP FROM transactions;");
+				upST.executeUpdate("UPDATE transactions SET nombreRestant = nombreRestant - " + retraitBons + " WHERE idTrans = " + rs.getString("idTrans"));
+				upST.executeUpdate("UPDATE users SET argent = argent - " + ((100-rs.getInt("prix")) * retraitBons) + " WHERE idUser = " + rs.getString("userID"));
+				upST.executeUpdate("UPDATE users SET argent = argent - " + (rs.getInt("prix") * retraitBons) + " WHERE idUser = " + idUser);
+				nbBons		-= retraitBons;
+				if(nbBons == 0)
+				    break;
+			    } while(rs.next());
+			    if(nbBons != 0)
+				st.executeUpdate("INSERT INTO transactions SELECT MAX(idtrans)+1, " + req.getParameter("id") + ", " + idUser + ", " + nbBons + ", " + nbBons + ", " + req.getParameter("prixBons") + ", " + req.getParameter("choix") + ", CURRENT_TIMESTAMP FROM transactions;");
+			} else {
+			    st.executeUpdate("INSERT INTO transactions SELECT MAX(idtrans)+1, " + req.getParameter("id") + ", " + idUser + ", " + req.getParameter("nbBons") + ", " + req.getParameter("nbBons") + ", " + req.getParameter("prixBons") + ", " + req.getParameter("choix") + ", CURRENT_TIMESTAMP FROM transactions;");
+			}
+			con.close();
+			res.sendRedirect("information?id=" + req.getParameter("id") + "&choix=" + req.getParameter("choix") + "&success=1");
+		    } else {
+			con.close();
+			res.sendRedirect("information?id=" + req.getParameter("id") + "&choix=" + req.getParameter("choix") + "&error=1");
+		    }
+		} catch (Exception e ) {
+		    e.printStackTrace(res.getWriter());
+		}			
+	    }
+    }
 }	
--- WEB-INF/sources/changerPerso.java
+++ WEB-INF/sources/changerPerso.java
@@ -12,57 +12,57 @@ import java.util.regex.Matcher;
 public class changerPerso extends HttpServlet
 {
 	public void service( HttpServletRequest req, HttpServletResponse res )
-		throws ServletException, IOException
-	{
-		String ancienPass 	= req.getParameter("ancienPass");
-		String nouveauPass 	= req.getParameter("nouveauPass");
-		String repetePass 	= req.getParameter("repetePass");
+	    throws ServletException, IOException
+    {
+	String ancienPass 	= req.getParameter("ancienPass");
+	String nouveauPass 	= req.getParameter("nouveauPass");
+	String repetePass 	= req.getParameter("repetePass");
 
-		String mail			= req.getParameter("mail");
+	String mail			= req.getParameter("mail");
 
-		try {
-			Context initCtx = new InitialContext();
-		    Context envCtx  = (Context) initCtx.lookup("java:comp/env");
-		    DataSource ds   = (DataSource) envCtx.lookup("base");
-		    Connection con  = ds.getConnection();
+	try {
+	    Context initCtx = new InitialContext();
+	    Context envCtx  = (Context) initCtx.lookup("java:comp/env");
+	    DataSource ds   = (DataSource) envCtx.lookup("base");
+	    Connection con  = ds.getConnection();
 
-		    Statement st    = con.createStatement();
-			ResultSet rs 	= st.executeQuery("SELECT pass, mail FROM users WHERE login='" + req.getUserPrincipal().getName() + "';");
-			rs.next();
+	    Statement st    = con.createStatement();
+	    ResultSet rs 	= st.executeQuery("SELECT pass, mail FROM users WHERE login='" + req.getUserPrincipal().getName() + "';");
+	    rs.next();
 
-			String redirect	= "perso",
-				   update 	= "UPDATE users SET ";
+	    String redirect	= "perso",
+		update 	= "UPDATE users SET ";
 
-			if(ancienPass!=null && !ancienPass.equals("")) {
-				if(ancienPass.equals(rs.getString("pass"))) {
-					if(nouveauPass.equals(repetePass)) {
-						update   += "pass='" + nouveauPass + "'";
-						redirect = "perso?success=1";
-					} else
-						redirect = "changePerso?error=2";
-				} else
-					redirect = "changePerso?error=1";
-			}
+	    if(ancienPass!=null && !ancienPass.equals("") && !nouveauPass.equals("")) {
+		if(ancienPass.equals(rs.getString("pass"))) {
+		    if(nouveauPass.equals(repetePass)) {
+			update   += "pass='" + nouveauPass + "'";
+			redirect = "perso?success=1";
+		    } else
+			redirect = "changePerso?error=2";
+		} else
+		    redirect = "changePerso?error=1";
+	    }
 
-			if( mail!=null && !mail.equals("") && !mail.equals(rs.getString("mail")) ) {
-				Pattern p = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$");
-				Matcher m = p.matcher(mail.toUpperCase());
-				if(m.matches()) {
-					if(update.charAt(update.length()-1) == '\'')
-						update += ", ";
-					update += " mail='" + mail + "'";
-					redirect = "perso?success=1";
-				} else
-					redirect = "changePerso?error=3";
-			}
+	    if( mail!=null && !mail.equals("") && !mail.equals(rs.getString("mail")) ) {
+		Pattern p = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$");
+		Matcher m = p.matcher(mail.toUpperCase());
+		if(m.matches()) {
+		    if(update.charAt(update.length()-1) == '\'')
+			update += ", ";
+		    update += " mail='" + mail + "'";
+		    redirect = "perso?success=1";
+		} else
+		    redirect = "changePerso?error=3";
+	    }
 
-			update += " WHERE login='" + req.getUserPrincipal().getName() + "';";
+	    update += " WHERE login='" + req.getUserPrincipal().getName() + "';";
 
-			if(!update.equals("UPDATE users SET  WHERE login='" + req.getUserPrincipal().getName() + "';"))
-				st.executeUpdate(update);
-			res.sendRedirect(redirect);
-		} catch (Exception e) {
-			e.printStackTrace(res.getWriter());
-		}
+	    if(!update.equals("UPDATE users SET  WHERE login='" + req.getUserPrincipal().getName() + "';"))
+		st.executeUpdate(update);
+	    res.sendRedirect(redirect);
+	} catch (Exception e) {
+	    e.printStackTrace(res.getWriter());
 	}
+    }
 }
